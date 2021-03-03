689a8744de6b23171c4e95b9a074aceac1eeec5e
Dan Roden <dan.roden@gmail.com>
2021-03-03 10:34:17 -0800

    add a table

--- src/js/views/EarthQuakeList.js
+++ src/js/views/EarthQuakeList.js
@@ -3,19 +3,32 @@ import { Link } from 'react-router-dom'
 import earthQuakeData from '../../data/earthquakes.json'
 
 export default function EarthQuakeList() {
-    const listItems = earthQuakeData.data.features.map((earthquake) => {
-        return (
-            <li key={earthquake.id}>
-                <Link to={`/detail/${earthquake.id}`}>
-                    {earthquake.properties.place}</Link>
-            </li>
-        )
-    });
 
-    console.log(`listItems ${listItems}`)
-    return (<>
-        <ul>
-            {listItems}
-        </ul>
-    </>)
+
+  const tableData = earthQuakeData.data.features.map((earthquake) => {
+    return (
+      <tr key={earthquake.id}>
+        <td data-label="Title">
+          <Link to={`/detail/${earthquake.id}`}>
+            {earthquake.properties.place}</Link>
+        </td>
+        <td data-label="Magnitude">{earthquake.properties.mag}</td>
+        <td data-label="Time">{earthquake.properties.time}</td>
+      </tr>
+    )
+  });
+  return (<>
+    <table>
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Magnitude</th>
+          <th>Time</th>
+        </tr>
+      </thead>
+      <tbody>
+        {tableData}
+      </tbody>
+    </table>
+  </>)
 }
