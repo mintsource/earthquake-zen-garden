1651803c54ccedf0af3c3975fb119e6727f62326
Dan Roden <dan.roden@gmail.com>
2021-03-03 17:44:13 -0800

    add basic sorting

--- src/js/views/EarthQuakeList.js
+++ src/js/views/EarthQuakeList.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom'
 import earthQuakeData from '../../data/earthquakes.json'
 
 export default function EarthQuakeList() {
 
+  const [sortedField, setSortedField] = useState()
+  const [sortAscending, setSortAscending] = useState()
+  const [sortedData, setSortedData] = useState(earthQuakeData.data.features)
 
-  const tableData = earthQuakeData.data.features.map((earthquake) => {
+  console.log(`sortedField: ${sortedField}`)
+
+  if(sortedField === undefined) {
+    sortField("place")
+  }
+
+  const tableData = sortedData.map((earthquake) => {
     return (
       <tr key={earthquake.id}>
-        <td data-label="Title">
+        <td data-label="Title" className="primary">
           <Link to={`/detail/${earthquake.id}`}>
             {earthquake.properties.place}</Link>
         </td>
@@ -17,13 +26,37 @@ export default function EarthQuakeList() {
       </tr>
     )
   });
+
+  function sortField(name) {
+    console.log("sortField:" + JSON.stringify(name))
+
+    if(name === sortedField) {
+      setSortAscending(!sortAscending)
+    } else {
+      setSortAscending(true)
+    }
+
+    sortedData.sort((a, b) => {
+      if (a.properties[name] < b.properties[name]) {
+        return sortAscending ? 1 : -1
+      }
+      if (a.properties[name] > b.properties[name]) {
+        return sortAscending ? -1 : 1
+      }
+      return 0
+    })
+
+    setSortedField(name)
+    setSortedData(sortedData)
+  }
+
   return (<>
-    <table>
+    <table className="earthquake-list">
       <thead>
         <tr>
-          <th>Title</th>
-          <th>Magnitude</th>
-          <th>Time</th>
+          <th onClick={() => sortField('place')} className={sortedField === 'place' ? 'sorted' : null}>Title</th>
+          <th onClick={() => sortField('mag')} className={sortedField === 'mag' ? 'sorted' : null}>Magnitude</th>
+          <th onClick={() => sortField('time')} className={sortedField === 'time' ? 'sorted' : null}>Time</th>
         </tr>
       </thead>
       <tbody>
